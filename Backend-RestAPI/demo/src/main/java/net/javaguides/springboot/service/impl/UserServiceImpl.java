740cac22f57dc7623039bdb6024555e1cf25661d
GAVASKAR <gavaskar07@gmail.com>
2023-05-04 09:37:04 +0530

    BackendRestAPI Added

---
+++ Backend-RestAPI/demo/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package net.javaguides.springboot.service.impl;
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.repository.UserRepository;
+import net.javaguides.springboot.service.UserService;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User updateUser(User user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+        return updatedUser;
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
