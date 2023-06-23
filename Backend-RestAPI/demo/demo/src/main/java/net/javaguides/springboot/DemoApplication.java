5f90d2a055fa915966d808d3af2e3b28c41efc8c
gishalinrufina <gishalin.rufina@gmail.com>
2023-06-23 23:27:11 +0530

    updated

--- Backend-RestAPI/demo/demo/src/main/java/net/javaguides/springboot/DemoApplication.java
+++ Backend-RestAPI/demo/demo/src/main/java/net/javaguides/springboot/DemoApplication.java
@@ -2,9 +2,9 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-@EnableEurekaClient
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
